62f7b986fe560b7948a1d84d9c4d30c48756dd67
Sekvojak <dominikkontrik123@gmail.com>
2026-03-08 18:56:01 +0100

    Add backend Spring project and project structure

---
+++ backend/src/main/java/com/theatre/backend/TheatreTicketSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.theatre.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreTicketSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheatreTicketSystemBackendApplication.class, args);
+	}
+
+}
